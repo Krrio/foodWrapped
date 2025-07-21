a91838c67a4d62729bc135024d68496cc431d77f
Kacper Jóźwik <42322656+Krrio@users.noreply.github.com>
2025-07-21 14:54:43 +0200

    feat: integrate CircularStd fonts

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,28 +1,59 @@
 import localFont from 'next/font/local';
 
-export const circularRegular = localFont({
-  src: '../public/fonts/circular-std-2.ttf',
-  weight: '400',
-  variable: '--font-circular-regular',
-});
-
-export const circularBold = localFont({
-  src: '../public/fonts/circular-std-4.ttf',
-  weight: '700',
-  variable: '--font-circular-bold',
-});
-
-// Combined variable for convenience when applying both font weights globally
+// Single font family containing all weights and styles of CircularStd
 export const circularStd = localFont({
   src: [
     {
-      path: '../public/fonts/circular-std-2.ttf',
-      weight: '400',
+      path: '../public/fonts/CircularStd-Light.otf',
+      weight: '300',
+      style: 'normal',
     },
     {
-      path: '../public/fonts/circular-std-4.ttf',
+      path: '../public/fonts/CircularStd-Light Italic.otf',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: '../public/fonts/CircularStd-Book.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/CircularStd-BookItalic.otf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../public/fonts/CircularStd-Medium.otf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/CircularStd-MediumItalic.otf',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: '../public/fonts/CircularStd-Bold.otf',
       weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/CircularStd-BoldItalic.otf',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: '../public/fonts/CircularStd-Black.otf',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/CircularStd-BlackItalic.otf',
+      weight: '900',
+      style: 'italic',
     },
   ],
   variable: '--font-circular',
+  display: 'swap',
 });
